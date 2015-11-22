0dc407fab610e074a92203bddc49fec2c8ab0281
Andreas Ahammer <andreas.ahammer@gmail.com>
2015-11-22 19:07:38 +0100

    create boatpos-util with Producer for Logger

---
+++ boatpos-server/boatpos-util/src/test/java/org/boatpos/util/log/LogTest.java
@@ -0,0 +1,27 @@
+package org.boatpos.util.log;
+
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+
+import javax.inject.Inject;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(Arquillian.class)
+public class LogTest {
+
+    @Inject
+    @SLF4J
+    private Logger log;
+
+    @Inject
+    private LogProducer logProducer;
+
+    @Test
+    public void testLog() {
+        assertNotNull(log);
+        assertNotNull(logProducer.produce(LogTest.class));
+    }
+}
