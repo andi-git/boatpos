aa26d82723bbb720d555f13d96c43048bf8be741
andreas.ahammer@gmail.com <andreas.ahammer@gmail.com>
2015-11-22 15:20:21 +0100

    add getter and setter in AbstractEntity

--- boatpos-server/boatpos-server-model/src/main/java/org/boatpos/model/AbstractEntity.java
+++ boatpos-server/boatpos-server-model/src/main/java/org/boatpos/model/AbstractEntity.java
@@ -1,19 +1,39 @@
 package org.boatpos.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Version;
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 /**
  * Basic class for all entities with an {@link #id} and a {@link #version}.
  */
+@MappedSuperclass
 public abstract class AbstractEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @NotNull
+    @Min(0)
     private Long id;
 
     @Version
+    @NotNull
+    @Min(0)
     private Integer version;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
 }
